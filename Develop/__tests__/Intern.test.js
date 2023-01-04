c45011c26f641ae7a6ade6e4a905d332ab181cc7
Sam Craig <samfcraig@gmail.com>
2023-01-03 22:38:46 -0500

    finished all tests

--- Develop/__tests__/Intern.test.js
+++ Develop/__tests__/Intern.test.js
@@ -1,7 +1,7 @@
 const Intern = require("../lib/Intern");
 
 describe("Interns are employees who are still in school", () => {
-    const intern = new Intern('Sam,' 0155, 'shmuushmuu@gmail.com', '1203456789');
+    const intern = new Intern('Sam', 0155, 'shmuushmuu@gmail.com', 'UNCC');
 
-    expect(engineer.gitHub).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
 });
--- Develop/__tests__/Manager.test.js
+++ Develop/__tests__/Manager.test.js
@@ -1,6 +1,8 @@
 const Manager = require("../lib/Manager");
 
-describe("Manager", () => {
+describe("Managers are employees who have an office phone number", () => {
+const manager = new Manager('Sam', 0155, 'shmuushmuu@gmail.com', '0987654321');
 
+expect(manager.officeNumber).toEqual(expect.any(String));
 });
 
