1928c007e4bfd066d2138c0ab4a56cd2c3109fac
Sam Craig <samfcraig@gmail.com>
2023-01-03 19:30:15 -0500

    added what is required

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHtml = require('./src/page-template');
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
