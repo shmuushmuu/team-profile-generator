7afca86b8d3b5ba79b1f8477a4e27e8e1ac01e65
Sam Craig <samfcraig@gmail.com>
2023-01-03 19:40:00 -0500

    added generate html and write file functions

--- Develop/index.js
+++ Develop/index.js
@@ -5,3 +5,23 @@ const generateHtml = require('./src/page-template');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const
+
+const writeFile = data => {
+    fs.writeFile('newHTML.html', data, err => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Now that you know your team better, make sure you don`t say anything offensive! :)')
+        }
+    })
+};
+
+questions() 
+.then(answers => { 
+    return generateHTML(answers);
+})
+.then(data => {
+    return writeFile(data);
+});
